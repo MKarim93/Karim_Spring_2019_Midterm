4383a40894920a66ea8cf46a6eb614ea3b394d2a
MKarim93 <mohammadekarim14@gmail.com>
2019-04-27 18:02:57 -0400

    completed DataReader class

--- src/datastructure/DataReader.java
+++ src/datastructure/DataReader.java
@@ -1,8 +1,14 @@
 package datastructure;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Stack;
+
 public class DataReader {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /*
          * User API to read the below textFile and print to console.
          * Use BufferedReader class.
@@ -18,9 +24,33 @@ public class DataReader {
          * Use For Each loop/while loop/Iterator to retrieve data.
          */
 
-        String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car.txt";
+        String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car";
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(textFile));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
+        String text;
+        while ((text = br.readLine()) != null) {
+            System.out.println(text);
+        }
 
+        Stack<String> wordMap = new Stack<String>();
+        String[] splitWords = textFile.split(" ");
+
+        for (String word : splitWords) {
+            Integer count = wordMap.indexOf(splitWords);
+            if (count == null) {
+                count = 0;
+            }
+
+            wordMap.add(word);
+        }
+
+        wordMap.push(textFile);
+        System.out.println(wordMap);
     }
 
 }
