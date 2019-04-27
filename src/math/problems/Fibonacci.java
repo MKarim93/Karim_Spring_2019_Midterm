3da533135b08c16f9ca1acbf58c7ad8892c2ae52
MKarim93 <mohammadekarim14@gmail.com>
2019-04-27 15:51:26 -0400

    finished Fibonacci class

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -6,7 +6,20 @@ public class Fibonacci {
           Write 40 Fibonacci numbers with java.
           0,1,1,2,3,5,8,13
          */
+Fibonacci fibonacci = new Fibonacci();
+fibonacci.fibonacci(40);
 
-
+    }
+    public void fibonacci(int x){
+        int a = 0;
+        int b = 1;
+        int c = a + b;
+        System.out.println("Fibonacci numbers till " + x + " are : ");
+        for (int i = 0; i<= x; i++){
+            System.out.print(a + " ");
+            a = b;
+            b = c;
+            c = a + b;
+        }
     }
 }
