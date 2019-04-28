d226bbda41273517fbe8ef42c4ad698c5a52952a
MKarim93 <mohammadekarim14@gmail.com>
2019-04-28 00:50:43 -0400

    completed DuplicateWord class

--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -1,5 +1,8 @@
 package string.problems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DuplicateWord {
 
     public static void main(String[] args) {
@@ -9,7 +12,23 @@ public class DuplicateWord {
          */
 
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+        int count;
+        st = st.toLowerCase();
+        String words[] = st.split(" ");
+
+        System.out.println("Duplicate words in a given string: ");
+        for (int i = 0; i < words.length; i++) {
+            count = 1;
+            for (int j = i + 1; j < words.length; j++) {
+                if (words[i].equals(words[j])) {
+                    count++;
+                    words[j] = "O";
+                }
+            }
+            if (count > 1 && words[i] != "O")
+                System.out.println(words[i]);
+        }
 
     }
-
 }
+
