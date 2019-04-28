236b07dea28fda29491ac5c2be098a95a53cd6df
MKarim93 <mohammadekarim14@gmail.com>
2019-04-27 23:55:48 -0400

    completed PrimeNumber class

--- src/math/problems/PrimeNumber.java
+++ src/math/problems/PrimeNumber.java
@@ -12,6 +12,22 @@ public class PrimeNumber {
          * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
          *
          */
+        int i;
+        int number;
+        String primeNumbers = "";
+        for (i = 2; i <= 1000000; i++) {
+            int counter = 0;
+            for (number = i; number >= 2; number--) {
+                if (i % number == 0) {
+                    counter = counter + 1;
+                }
+                if (counter == 2) {
+                    primeNumbers = primeNumbers + i + " ";
+                }
+            }
+            System.out.println("Prime numbers 2 to 1000000 are: ");
+            System.out.println(primeNumbers);
+        }
 
     }
 
